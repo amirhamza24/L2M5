d445d3fd60362b4c41f92a3497c8ba3ee66f8839
amirhamza24 <ridoykhan7897890@gmail.com>
2026-01-06 03:13:23 +0600

    add search multiple fields

--- Module_23/src/modules/post/post.service.ts
+++ Module_23/src/modules/post/post.service.ts
@@ -1,4 +1,5 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (
@@ -15,28 +16,74 @@ const createPost = async (
   return result;
 };
 
-const getAllPost = async (payload: { search?: string | undefined }) => {
-  const allPost = await prisma.post.findMany({
-    where: {
+const getAllPost = async ({
+  search,
+  tags,
+  isFeatured,
+  status,
+  authorId,
+}: {
+  search?: string | undefined;
+  tags: string[] | [];
+  isFeatured?: boolean | undefined;
+  status: PostStatus | undefined;
+  authorId: string | undefined;
+}) => {
+  const andConditions: PostWhereInput[] = [];
+
+  if (search) {
+    andConditions.push({
       OR: [
         {
           title: {
-            contains: payload.search as string,
+            contains: search,
             mode: "insensitive",
           },
         },
         {
           content: {
-            contains: payload.search as string,
+            contains: search,
             mode: "insensitive",
           },
         },
         {
           tags: {
-            has: payload.search as string,
+            has: search,
           },
         },
       ],
+    });
+  }
+
+  if (tags.length > 0) {
+    andConditions.push({
+      tags: {
+        hasEvery: tags as string[],
+      },
+    });
+  }
+
+  if (typeof isFeatured === "boolean") {
+    andConditions.push({
+      isFeatured,
+    });
+  }
+
+  if (status) {
+    andConditions.push({
+      status,
+    });
+  }
+
+  if (authorId) {
+    andConditions.push({
+      authorId,
+    });
+  }
+
+  const allPost = await prisma.post.findMany({
+    where: {
+      AND: andConditions,
     },
   });
   return allPost;
